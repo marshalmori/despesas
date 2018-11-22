a1737f10dfcc598b48645dd7a2dc2ba1e613dd22
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2018-11-22 13:52:11 -0200

    feat: Ler as entradas de dados: tipo, descrição e valor das despesas.

--- app.js
+++ app.js
@@ -8,15 +8,39 @@ var budgetController = (function () {
 // UI Controller
 var UIController = (function(){
 
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  };
+
+  return{
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
+
 })();
 
 
 
 //Global App controller
 var controller = (function(budgetCtrl, UICtrl){
+  var DOM = UICtrl.getDOMstrings();
 
   var ctrlAddItem = function(){
     // 1. Get the field input data
+    var input = UICtrl.getInput();
+    console.log(input);
 
     // 2. Add the item to the budget Controller
 
@@ -25,11 +49,11 @@ var controller = (function(budgetCtrl, UICtrl){
     // 4. Calculate the budget
 
     // 5. Display the budget on the UI
-    
+
 
   }
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', function(event){
     if(event.keyCode === 13 || event.which === 13){
