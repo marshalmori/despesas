0c0568d1316c6adecb8cb66a1f1d0bac92af27da
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2018-11-26 16:29:33 -0200

    feat: Criar a estrutura para deletar uma entrada com Event Listener utilizando Event Delegation.

--- app.js
+++ app.js
@@ -109,7 +109,8 @@ var UIController = (function(){
     budgetLabel: '.budget__value',
     incomeLabel: '.budget__income--value',
     expenseLabel: '.budget__expenses--value',
-    percentageLabel: '.budget__expenses--percentage'
+    percentageLabel: '.budget__expenses--percentage',
+    container: '.container'
   };
 
   return{
@@ -196,6 +197,9 @@ var controller = (function(budgetCtrl, UICtrl){
         ctrlAddItem();
       }
     });
+
+    document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
   };
 
 
@@ -232,9 +236,30 @@ var controller = (function(budgetCtrl, UICtrl){
       updateBudget();
     }
 
+  };
 
+  var ctrlDeleteItem = function(event) {
+    var itemID, splitID, type, ID;
+
+    itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
+    if (itemID) {
+      //income-1
+      splitID = itemID.split('-'); // separa income-1 em um array ['income', '1']
+      type = splitID[0];
+      ID = splitID[1];
+
+      // 1. Delete the item form the data structure
+
+
+      // 2. Delete the itm from the UI
+
+      // 3. Update and show the new budget
+
+    }
 
   };
+
 
   return {
     init: function() {
