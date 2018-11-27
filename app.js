18369b98a14ac03bcade128d106ad83c18e51f27
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2018-11-26 23:34:42 -0200

    feat: Manipular as cores nos inputs e no botão de inserir dadas de acordo com o type, vermelho se saída e verde se entrada.

--- app.js
+++ app.js
@@ -174,6 +174,12 @@ var UIController = (function(){
     return (type === 'expense' ? '-' : '+') + ' ' + int + '.' + dec;
   };
 
+  var nodeListForEach = function(list, callback){
+    for (var i = 0; i < list.length; i++){
+      callback(list[i], i);
+    }
+  };
+
 
   return{
     getInput: function() {
@@ -246,11 +252,7 @@ var UIController = (function(){
 
       var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-      var nodeListForEach = function(list, callback){
-        for (var i = 0; i < list.length; i++){
-          callback(list[i], i);
-        }
-      };
+
 
       nodeListForEach(fields, function(current, index){
 
@@ -274,6 +276,20 @@ var UIController = (function(){
       year = now.getFullYear();
       document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
 
+    },
+
+    changedType: function(){
+
+      var fields = document.querySelectorAll(
+        DOMstrings.inputType + ',' +
+        DOMstrings.inputDescription + ',' +
+        DOMstrings.inputValue);
+
+        nodeListForEach(fields, function(cur){
+          cur.classList.toggle('red-focus');
+        });
+
+        document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
     },
 
     getDOMstrings: function(){
@@ -300,6 +316,8 @@ var controller = (function(budgetCtrl, UICtrl){
     });
 
     document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+    document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
 
   };
 
