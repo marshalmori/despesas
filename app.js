99e172f554d0bb9d9e717afcf3159d5829f05a5a
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2018-11-26 23:01:37 -0200

    feat: Formatar os números e inserir o sinal de positivo ou negativo na frente do valor.

--- app.js
+++ app.js
@@ -155,6 +155,25 @@ var UIController = (function(){
 
   };
 
+  var formatNumber = function(num, type){
+    var numSplit;
+
+    num = Math.abs(num);
+    num = num.toFixed(2);
+
+    numSplit = num.split('.');
+
+    int = numSplit[0];
+    if (int.length > 3) {
+      int = int.substr(0 , int.length - 3) + ',' + int.substr(int.length - 3, int.length);
+    }
+
+    dec = numSplit[1];
+
+    return (type === 'expense' ? '-' : '+') + ' ' + int + '.' + dec;
+  };
+
+
   return{
     getInput: function() {
       return {
@@ -179,7 +198,7 @@ var UIController = (function(){
       // Replace the placeholder text with some actual data
       newHtml = html.replace('%id%', obj.id);
       newHtml = newHtml.replace('%description%', obj.description);
-      newHtml = newHtml.replace('%value%', obj.value);
+      newHtml = newHtml.replace('%value%', formatNumber(obj.value, type));
 
       // Insert the HTML into the DOM
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
@@ -207,9 +226,12 @@ var UIController = (function(){
     },
 
     displayBudget: function(obj) {
-      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
-      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
-      document.querySelector(DOMstrings.expenseLabel).textContent = obj.totalExp;
+      var type;
+      obj.budget > 0 ? type = 'income' : type = 'expense';
+
+      document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget, type);
+      document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalInc, 'income');
+      document.querySelector(DOMstrings.expenseLabel).textContent = formatNumber(obj.totalExp, 'expense');
 
       if(obj.percentage > 0){
         document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
